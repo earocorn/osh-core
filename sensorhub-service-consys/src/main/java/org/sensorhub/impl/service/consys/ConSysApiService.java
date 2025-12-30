5effe7c73c6d810a3457db28bdea2da642e364da
Alex Almanza <alexalmanza@Alexs-MacBook-Pro.local>
2025-12-29 20:59:55 -0600

    Increase CSAPI threadpool size to availableProcessors * 4 since CSAPI work is mainly I/O bound

--- sensorhub-service-consys/src/main/java/org/sensorhub/impl/service/consys/ConSysApiService.java
+++ sensorhub-service-consys/src/main/java/org/sensorhub/impl/service/consys/ConSysApiService.java
@@ -158,7 +158,7 @@ public class ConSysApiService extends AbstractHttpServiceModule<ConSysApiService
         
         // init thread pool
         threadPool = Executors.newScheduledThreadPool(
-            Runtime.getRuntime().availableProcessors(),
+            Runtime.getRuntime().availableProcessors() * 4,
             new NamedThreadFactory("CSApi-Pool"));
 
         // init timeout monitor
